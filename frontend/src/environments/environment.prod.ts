5c9bc94f3743edc578c7a6bbaa06fca1ae8e3967
chahd2004 <chahdhachani118@gmail.com>
2026-06-07 12:34:08 +0100

    fix: use ngrok URL for backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mon-projet-hban.onrender.com/api',
-  qrBaseUrl: 'https://mon-projet-hban.onrender.com'
+  apiUrl: 'https://mayson-loculate-sadie.ngrok-free.dev/api',
+  qrBaseUrl: 'https://mayson-loculate-sadie.ngrok-free.dev'
 };
